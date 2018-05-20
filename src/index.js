5bb7601b9e8e83e2c53285d248c87c29b4ec7ec5
Andrey Ovcharenko <ovcharenkoav@meta.ua>
2018-05-20 17:26:06 +0300

    re-render fix

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { hydrate } from 'react-dom';
-
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-const cache = new InMemoryCache().restore(window.__APOLLO_STATE__);
-
 const client = new ApolloClient({
-  uri: 'https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex',
-  cache,
-  //defaultOptions: {
-  //  query: {
-  //    fetchPolicy: "cache-first",
-  //  },
-  //},
-  // initialState: window.__APOLLO_STATE__,
-  // ssrForceFetchDelay: 1000000000,
-  // ssrCacheFirstFetchPolicyDuration: 1000000000,
+  link: ApolloLink.from([
+     onError(({ graphQLErrors, networkError }) => {
+       if (graphQLErrors)
+         graphQLErrors.map(({ message, locations, path }) =>
+           console.log(
+             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+           ),
+         );
+       if (networkError) console.log(`[Network error]: ${networkError}`);
+     }),
+     new HttpLink({
+       uri: 'https://api.graph.cool/simple/v1/ciyz901en4j590185wkmexyex',
+       // credentials: 'same-origin'
+     })
+  ]),
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
-console.log(cache)
-
-
 
 hydrate(<App client={client} />, document.getElementById('app'));
-// registerServiceWorker();
